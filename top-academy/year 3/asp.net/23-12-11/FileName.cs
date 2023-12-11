d06e39cea27019bbd7714df7d7035740ea8aea8f
Eugene Veselov <mentalsolin@proton.me>
2023-12-11 18:51:29 +0200

    Добавление 23-12-11 Turtle

---
+++ top-academy/year 3/asp.net/23-12-11/FileName.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_12_11
+{
+    public class FileName
+    {
+        SqlConnection conn;
+        public FileName()
+        {
+            conn = new SqlConnection();
+            string connectionString =
+                @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+        }
+        static void Main(string[] args)
+        {
+            FileName pr = new FileName();
+        }
+        public void InsertQuery()
+        {
+            try
+            {
+                conn.Open();
+                string insertString = @"insert into Authors (FirstName, LastName) values ('Roger', 'Zelazny')";
+                SqlCommand cmd = new SqlCommand(insertString, conn);
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ top-academy/year 3/python/23-12-11 Turtle/Turtle.py
@@ -0,0 +1,61 @@
+import turtle
+# turtle.bgcolor("blue")
+
+# #1st turtle
+# t = turtle.Pen()
+# t.shape("turtle")
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+
+# turtle.shape("turtle")
+
+# #2nd turtle
+# turtle.shape("turtle")
+# turtle.color("green","green")
+# turtle.pensize(10)
+# turtle.right(45)
+# turtle.backward(250)
+
+# size = int(turtle.numinput("Enter size: "," in pixels", 40, 10,80))
+# turtle.pensize(size)
+# turtle.right(80)
+# turtle.forward(100)
+
+#3rd turle
+
+t2 = turtle.Pen()
+
+t2.left(90)
+t2.penup()
+t2.forward(50)
+t2.pendown()
+t2.circle(100, 180)
+t2.forward(200)
+t2.right(90)
+t2.circle(25, 180)
+t2.forward(200)
+t2.circle(25, 180)
+t2.forward(200)
+t2.right(180)
+t2.penup()
+t2.forward(200)
+t2.left(90)
+t2.pendown()
+t2.forward(50)
+t2.right(45)
+t2.forward(150)
+t2.circle(50, 180)
+t2.forward(50)
+t2.penup()
+t2.forward(220)
+t2.right(90)
+t2.right(0)
+t2.pendown()
+t2.right(45)
+t2.forward(160)
+
+
+
+
+turtle.done()
