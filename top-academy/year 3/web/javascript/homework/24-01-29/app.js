dfafc397ab51b21c8f592465166178b502f18c5d
Eugene Veselov <eugene.veselov@outlook.com>
2024-02-11 16:24:25 +0200

    Добавление ДЗ "24-01-29"

--- top-academy/year 3/web/javascript/homework/24-01-29/app.js
+++ top-academy/year 3/web/javascript/homework/24-01-29/app.js
@@ -1,17 +1,27 @@
-// let name = prompt("pls enter yr name here")
-// alert("oh, hi " + name + "!")
+// Задание 1.
+let name = prompt("please enter you're name here")
+alert("oh, hi " + name + "!")
 
+// Задание 2.
+let date = new Date()
+let year = prompt("can you write the year you were born")
 
+if (year >= 1850) {
+    alert("you are... " + (date.getFullYear() - year))
+} else {
+    alert("WHY YOU ARE STILL AVILE?!")
+}
 
-// let date = new Date()
-// let year = prompt("can you type year were you born")
-
-// if (year >= 1850) {
-//     alert("you are... " + (date.getFullYear() - year))
-// } else {
-//     alert("WHY YOU ARE STILL AVILE?!")
-// }
-
-
-
+// Задание 3.
 let lenght = prompt("enter lenght side of a rectangle")
+alert('S = ' + length * lenght)
+
+// Задание 4.
+let r = prompt('enter the radius of the circle')
+alert('S = ' + r * r * Math.PI)
+
+// Задание 5.
+let dist = prompt("Enter distance between two cities in km")
+let time = prompt("How long do you want to get there? (hours like: 4.5 = 4:30)")
+
+alert(dist/time + " km/h")
